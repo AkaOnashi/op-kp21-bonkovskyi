a836f4808f7a5c1eed65682034fd2f014b687946
IlliaBonkovskyi <bonkovskyiillia@gmail.com>
2022-11-13 20:34:17 +0200

    Коміт другого завдання на етапі заглшок

---
+++ sem_1_lab_1/task2/task2/Task2.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Task2
+{
+    static void Main(string[] args)
+    {
+        int n;
+        Console.Write("This program checks if the number n is a prime.");
+        Console.WriteLine("Enter the value for n: ");
+        n = Convert.ToInt32(Console.ReadLine());
+    }
+}
