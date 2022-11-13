8e2475805ac2d979c85e3a5385459b31e9f465e3
IlliaBonkovskyi <bonkovskyiillia@gmail.com>
2022-11-14 00:29:14 +0200

    Завдання 4: етап заглушок

---
+++ sem_1_lab_1/Task4/Task4/Task4.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Task4
+{
+    static void Main(string[] args)
+    {
+        double x, n, a, sin_x;
+        Console.WriteLine("Enter the value for x: ");
+        x = Convert.ToDouble(Console.ReadLine());
+    }
+}
