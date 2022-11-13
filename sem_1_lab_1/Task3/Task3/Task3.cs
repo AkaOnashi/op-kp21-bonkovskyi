06ecf4d9052e92efe662f8926914d0f1f5c95316
IlliaBonkovskyi <bonkovskyiillia@gmail.com>
2022-11-13 22:28:54 +0200

    Завдання 3: повна реалізація

--- sem_1_lab_1/Task3/Task3/Task3.cs
+++ sem_1_lab_1/Task3/Task3/Task3.cs
@@ -5,25 +5,20 @@ class Task3
     static void Main(string[] args)
     {
         //	Написати програму для обчислення факторіалу числа n та значення x^n.
-        /*
-         test cases:
-        1: n = 1; x = 2
-        2: n = 3; x = 3
-        3: n = 8; x = 4
-        4: n = 0; x = 34
-         */
-        int n, x, fact_n, x_n;
+        int n, x, fact_n = 1, x_n;
         Console.Write("Enter the value for n: ");
         n = Convert.ToInt32(Console.ReadLine());
         Console.Write("Enter the value for x: ");
         x = Convert.ToInt32(Console.ReadLine());
 
-        /*
-        1: fact_n = 1; x_n = 2
-        2: fact_n = 6; x_n = 27
-        1: fact_n = 40320; x_n = 65536
-        1: fact_n = 1; x_n = 1
-         */
+        for (int i = 1; i <= n; i++)
+        {
+            fact_n = fact_n * i;
+        }
+        Console.WriteLine("The factorial of " + n + " is equal to " + fact_n);
+
+        x_n = (int)Math.Pow(x, n);
+        Console.WriteLine("The x to the nth power is equal to " + x_n);
 
     }
 }
