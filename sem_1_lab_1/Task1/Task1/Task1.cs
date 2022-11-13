86632a6100346a341b51e679961850d35c59374a
IlliaBonkovskyi <bonkovskyiillia@gmail.com>
2022-11-13 23:29:19 +0200

    Завдання 1: етап заглушок

---
+++ sem_1_lab_1/Task1/Task1/Task1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Task1
+{
+    static void Main(string[] args)
+    {
+        double x_0, x_n, n;
+        int x, y;
+        double a = 1.35;
+        double b = -6.25;
+        Console.Write("Enter the value for x_0: ");
+        x_0 = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Enter the value for x_n: ");
+        x_n = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Enter the value for n: ");
+        n = Convert.ToInt32(Console.ReadLine());
+    }
+}
