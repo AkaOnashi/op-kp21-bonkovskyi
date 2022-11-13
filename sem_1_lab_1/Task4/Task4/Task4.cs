a8ab0c63f26431a087fb410fd3eed0a034e77b3d
IlliaBonkovskyi <bonkovskyiillia@gmail.com>
2022-11-14 00:36:00 +0200

    Завдання 4: тестові сценарії. Описав табличні значення синусу для 10-ох довілних x

--- sem_1_lab_1/Task4/Task4/Task4.cs
+++ sem_1_lab_1/Task4/Task4/Task4.cs
@@ -4,8 +4,34 @@ class Task4
 {
     static void Main(string[] args)
     {
+        /*
+         1: x = 0
+         2: x = 1
+         3: x = 10
+         4: x = 5
+         5: x = 44
+         6: x = 675
+         7: x = -7
+         8: x = 24
+         9: x = 3
+        10: x = 25
+         */
         double x, n, a, sin_x;
         Console.WriteLine("Enter the value for x: ");
         x = Convert.ToDouble(Console.ReadLine());
+
+        /*
+         Табличні значення синусу
+         1: sin_x = 0
+         2: sin_x = 0.84
+         3: sin_ = -0.54
+         4: sin_ = -0.96
+         5: sin_ = 0.02
+         6: sin_ = 0.43
+         7: sin_ = -0.66
+         8: sin_ = -0.91
+         9: sin_ = 0.14
+        10: sin_ = -0.13
+         */
     }
 }
