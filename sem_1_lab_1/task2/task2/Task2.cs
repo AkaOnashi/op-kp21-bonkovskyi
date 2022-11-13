dd6a4cee94f4515a4008c1c15c19541f14824af6
IlliaBonkovskyi <bonkovskyiillia@gmail.com>
2022-11-13 20:42:28 +0200

    Завдання 2: тестові сценарії

--- sem_1_lab_1/task2/task2/Task2.cs
+++ sem_1_lab_1/task2/task2/Task2.cs
@@ -4,9 +4,27 @@ class Task2
 {
     static void Main(string[] args)
     {
+        /*
+         test cases
+        1: n = 2
+        2: n = 6
+        3: n = 13
+        4: n = 0
+         */
+
         int n;
         Console.Write("This program checks if the number n is a prime.");
         Console.WriteLine("Enter the value for n: ");
         n = Convert.ToInt32(Console.ReadLine());
+
+        /*
+         1: "n is the prime number"
+         2: "n is not the prime number"
+         3: "n is the prime number"
+         4: "n is not the prime number"
+        
+         */
+
+
     }
 }
