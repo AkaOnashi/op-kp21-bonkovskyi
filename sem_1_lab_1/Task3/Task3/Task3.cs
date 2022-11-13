178dab1c2785be4c8636ca8c708958d2c3aab3fc
IlliaBonkovskyi <bonkovskyiillia@gmail.com>
2022-11-13 22:13:06 +0200

    Завдання 3: тестові сценарії

--- sem_1_lab_1/Task3/Task3/Task3.cs
+++ sem_1_lab_1/Task3/Task3/Task3.cs
@@ -5,11 +5,25 @@ class Task3
     static void Main(string[] args)
     {
         //	Написати програму для обчислення факторіалу числа n та значення x^n.
-        int n, x;
+        /*
+         test cases:
+        1: n = 1; x = 2
+        2: n = 3; x = 3
+        3: n = 8; x = 4
+        4: n = 0; x = 34
+         */
+        int n, x, fact_n, x_n;
         Console.Write("Enter the value for n: ");
         n = Convert.ToInt32(Console.ReadLine());
         Console.Write("Enter the value for x: ");
         x = Convert.ToInt32(Console.ReadLine());
-        
+
+        /*
+        1: fact_n = 1; x_n = 2
+        2: fact_n = 6; x_n = 27
+        1: fact_n = 40320; x_n = 65536
+        1: fact_n = 1; x_n = 1
+         */
+
     }
 }
