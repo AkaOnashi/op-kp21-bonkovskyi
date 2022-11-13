f1fa8cacfdbe84f0b4d0554f4ae7e1ce92e8c9ec
IlliaBonkovskyi <bonkovskyiillia@gmail.com>
2022-11-13 21:26:41 +0200

    Завдання 2: повна реалізація

--- sem_1_lab_1/task2/task2/Task2.cs
+++ sem_1_lab_1/task2/task2/Task2.cs
@@ -12,11 +12,35 @@ class Task2
         4: n = 0
          */
 
-        int n;
-        Console.Write("This program checks if the number n is a prime.");
-        Console.WriteLine("Enter the value for n: ");
+        int n; 
+        Console.WriteLine("This program checks if the number n is a prime.");
+        Console.Write("Enter the value for n: ");
         n = Convert.ToInt32(Console.ReadLine());
-
+        bool flag = true;
+        if(n >= 2)
+        {
+            for (int i = 2; i <= n; i++)
+            {
+                if (n % i == 0)
+                {
+                    Console.WriteLine("The " + n + " is not the prime number");
+                    flag = false;
+                    break;
+                }
+                if (flag == true)
+                {
+                    Console.WriteLine("The " + n + " is the prime number");
+                    break;
+                }
+            }
+        }
+        else
+        {
+            Console.WriteLine("The " + n + " is not the prime number");
+        }
+        
+        
+        
         /*
          1: "n is the prime number"
          2: "n is not the prime number"
