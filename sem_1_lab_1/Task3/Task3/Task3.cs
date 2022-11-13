f7a39d5738c5174691c1de95ae07a9762d2531cd
IlliaBonkovskyi <bonkovskyiillia@gmail.com>
2022-11-13 21:54:12 +0200

    Завдання 3: етап заглушок

---
+++ sem_1_lab_1/Task3/Task3/Task3.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Task3
+{
+    static void Main(string[] args)
+    {
+        //	Написати програму для обчислення факторіалу числа n та значення x^n.
+        int n, x;
+        Console.Write("Enter the value for n: ");
+        n = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Enter the value for x: ");
+        x = Convert.ToInt32(Console.ReadLine());
+        
+    }
+}
