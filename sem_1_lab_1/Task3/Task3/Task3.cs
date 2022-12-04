cc4d35ebbf63cc376d31c94a91de55954206abff
IlliaBonkovskyi <bonkovskyiillia@gmail.com>
2022-12-04 23:02:53 +0200

    Завдання 3: багофікс1

--- sem_1_lab_1/Task3/Task3/Task3.cs
+++ sem_1_lab_1/Task3/Task3/Task3.cs
@@ -5,19 +5,19 @@ class Task3
     static void Main(string[] args)
     {
         //	Написати програму для обчислення факторіалу числа n та значення x^n.
-        int n, x, fact_n = 1, x_n;
+        ulong n, x, fact_n = 1, x_n;
         Console.Write("Enter the value for n: ");
-        n = Convert.ToInt32(Console.ReadLine());
+        n = ulong.Parse(Console.ReadLine());
         Console.Write("Enter the value for x: ");
-        x = Convert.ToInt32(Console.ReadLine());
+        x = ulong.Parse(Console.ReadLine());
 
-        for (int i = 1; i <= n; i++)
+        for (ulong i = 1; i <= n; i++)
         {
             fact_n = fact_n * i;
         }
         Console.WriteLine("The factorial of " + n + " is equal to " + fact_n);
 
-        x_n = (int)Math.Pow(x, n);
+        x_n = (ulong)Math.Pow(x, n);
         Console.WriteLine("The x to the nth power is equal to " + x_n);
 
     }
