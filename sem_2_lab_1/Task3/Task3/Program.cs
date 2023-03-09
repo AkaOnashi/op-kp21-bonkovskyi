267883e31ebaf999d330d65738be877adac52d2c
IlliaBonkovskyi <bonkovskyiillia@gmail.com>
2023-03-09 19:08:52 +0200

    Task3 v0.1

---
+++ sem_2_lab_1/Task3/Task3/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // The text file contains arbitrary English words, 1 word per line, no more than 40 words in the file,
+            // the word length is limited to 80 characters.Rewrite words to another file by sorting them alphabetically.
+
+            try
+            {
+                //new file & put down words in it
+                using (StreamWriter sw = new StreamWriter("EngWords.txt"))
+                {
+
+                }
+
+                // some cycle for sorting alphabetically
+                for ()
+                {
+
+                }
+
+                //rewritten file for sorted words
+                using(StreamWriter sw = new StreamWriter("SortedEngWords.txt"))
+                {
+
+                }
+            }
+            catch
+            {
+                Console.WriteLine("smth went wrong :(");
+            }
+
+        }
+
+        //method that will make random word
+        public static void WordGenerator(string word)
+        {
+            
+        }
+
+    }
+}
