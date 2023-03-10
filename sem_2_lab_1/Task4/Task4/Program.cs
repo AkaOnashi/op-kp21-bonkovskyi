1fe974f51d3db6b1578008f31bf8e69d13a567ef
IlliaBonkovskyi <bonkovskyiillia@gmail.com>
2023-03-10 20:08:40 +0200

    Task4 v1.0

--- sem_2_lab_1/Task4/Task4/Program.cs
+++ sem_2_lab_1/Task4/Task4/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 using System.IO;
 using static System.Formats.Asn1.AsnWriter;
 using static System.Net.WebRequestMethods;
@@ -17,24 +18,53 @@ namespace Task4
 
                 using (StreamReader sr = new StreamReader("C:\\Users\\user\\Desktop\\Students.csv"))
                 {
-                            //test cases
-                        //Input: file with studnets: name, last name, score
-                        //Output: students whom score is less than 60. So in console we have to see the next:
-                                // Students whose score is less than 60 points:
-                            //First Name 1  Last Name 1
-                            //First Name 4  Last Name 4
-                            //First Name 5  Last Name 5
-                            //First Name 8  Last Name 8
-                            //First Name 11  Last Name 11
+                    //test cases
+                    //Input: file with studnets: name, last name, score
+                    //Output: students whom score is less than 60. So in console we have to see the next:
+                    // Students whose score is less than 60 points:
+                    //First Name 1  Last Name 1
+                    //First Name 4  Last Name 4
+                    //First Name 5  Last Name 5
+                    //First Name 8  Last Name 8
+                    //First Name 11  Last Name 11
 
                     //convert data from file into array
-                    string students = sr.ReadToEnd();
+                    string text = sr.ReadToEnd();
+                    string[] stud = text.Split(new string[] {";"}, StringSplitOptions.RemoveEmptyEntries);
+                    string[,] arr = new string[12, 3];
+                    int a = 0;
+                    for(int i = 0; i < 12; i++)
+                    {
+                        for(int j = 0; j < 3; j++)
+                        {
+                            arr[i, j] = stud[a];
+                            a++;
+                        }
+                        
+                    }
 
                     // find student with score <60
+                    //display this student on the screen
+                    string stud_less = "";
+                    for (int i = 0; i < 12; i++)
+                    {
+                        int b = Int32.Parse(arr[i, 2]);
+                        if (b < 60)
+                        {
+                            stud_less += arr[i, 0] + arr[i, 1];
+                        }
+                    }
+
+                    if(stud_less == "")
+                        Console.WriteLine("There are not students whose score is less than 60");
+
+                    Console.WriteLine(stud_less);
+
+                    
 
                     //dasplay this student on the screen
-                    string[] stud_less = new string[students.Length];
-                    
+                    //string[] stud_less = new string[students.Length];
+
                 }
             }
 
