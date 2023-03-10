1b9b21d5b7420ca66487c2da98796c6f073da40c
IlliaBonkovskyi <bonkovskyiillia@gmail.com>
2023-03-10 21:37:27 +0200

    Task5 v1.0

---
+++ sem_2_lab_1/Task5/Task5/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+namespace Task5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a program that will calculate the number of each word in a text file.
+
+            using(StreamReader sr = new StreamReader("C:\\Users\\user\\Desktop\\text.txt"))
+            {
+                string words = sr.ReadToEnd();
+                string[] words1 = words.Split(new string[] { " ", ".", ",", "!", "?", ";", ":", "-", "(", ")", "[", "]", "{", "}", "/", "\\", "\'", "\"" }, StringSplitOptions.RemoveEmptyEntries);
+                int a = 0;
+
+                Console.WriteLine(words1.Length);
+            }
+
+        }
+    }
+}
