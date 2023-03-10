095005fb9323ff21ea0b54a5da74b2477a923127
IlliaBonkovskyi <bonkovskyiillia@gmail.com>
2023-03-10 16:49:05 +0200

    Task4 v0.1

---
+++ sem_2_lab_1/Task4/Task4/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using static System.Formats.Asn1.AsnWriter;
+using static System.Net.WebRequestMethods;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //The text file contains the names and surnames of the students and their scores in CSV format.
+            //Display all students whose score is less than 60 points.If such students are absent, display a message on the screen.
+
+            try
+            {
+
+                using (StreamReader sr = new StreamReader("C:\\Users\\user\\Desktop\\Students.csv"))
+                {
+                    //convert data from file into array
+                    string students = sr.ReadToEnd();
+
+                    // find student with score <60
+
+                    //dasplay this student on the screen
+                    string[] stud_less = new string[students.Length];
+                    
+                }
+            }
+
+            catch
+            {
+                Console.WriteLine("Some problems here!");
+            }
+        }
+    }
+}
