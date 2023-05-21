7964f1955dcbb75fe97be98f4ba834630914484e
IlliaBonkovskyi <bonkovskyiillia@gmail.com>
2023-05-21 23:52:22 +0300

    Task1 v1.0

--- sem_2_lab_2/Task1/Task1/Program.cs
+++ sem_2_lab_2/Task1/Task1/Program.cs
@@ -1,25 +1,70 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Good afternoon!\n Choose the option:");
-        Console.WriteLine(" 1 - Fill in information about the worker.\n 2 - See the info of a specific worker.\n 3 - See the general info. \n 4 - Stop the program.");
-        int choice = Convert.ToInt32(Console.ReadLine());
+        string last_name;
+        int num = 1; 
+        double salary, withheld, total_salary, sum_salary = 0, sum_withheld = 0, sum_total_salary = 0;
+        bool flag = true;
 
-        switch (choice)
+        List<SalaryInfo> workers = new List<SalaryInfo>();
+
+        Console.WriteLine("\nGood afternoon!");
+        while (flag)
         {
-            case 1: 
-                break;
-            case 2:
-                break;
-            case 3:
-                break;
-            case 4:
-                break;
-            default:
-                break;
+            Console.WriteLine("\n Choose the option:");
+            Console.WriteLine("\n 1 - Fill in information about the worker.\n 2 - See the general info \n 3 - Stop the program.");
+            Console.Write("\nEnter the number of option: ");
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    Console.Write("Enter the worker's last name: ");
+                    last_name = Console.ReadLine();
+                    Console.Write("Enter the worker's salary: ");
+                    salary = Convert.ToDouble(Console.ReadLine());
+                    Console.Write("Enter the worker's withheld: ");
+                    withheld = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("The information about {0} was filled. Thanks!", last_name);
+                    SalaryInfo worker = new SalaryInfo(last_name, salary, withheld);
+                    total_salary = worker.TotalSalary();
+                    worker.num = num;
+                    num++;
+                    workers.Add(worker);
+                    break;
+                
+                case 2:
+                    Console.WriteLine("\nGeneral information about the workers:");
+                    Console.WriteLine("\n Num - Last name - Salary,grn - Withheld,grn - Gived,grn");
+                    for(int i = 0; i < workers.Count; i++)
+                    {
+                        workers[i].PrintGeneralInfo();
+                    }
+
+                    // counting sum
+                    for (int i = 0; i < workers.Count; i++)
+                    {
+                        sum_salary += workers[i].Salary;
+                        sum_withheld += workers[i].Withheld;
+                        sum_total_salary += workers[i].Total_Salary;
+                    }
+
+                    Console.WriteLine("\n Total salary: {0} \n Total withheld: {1}\n Total wgived: {2}", sum_salary, sum_withheld, sum_total_salary);
+                    break;
+
+                case 3: 
+                    flag = false;
+                    Console.WriteLine("\nGoodbye!");
+                    break;
+                default:
+                    Console.WriteLine("Please, enter the number of option! ");
+                    break;
+            }
         }
     }
 
@@ -27,16 +72,15 @@ class Program
 
 public class SalaryInfo
 {
-    private int num;
+    public int num;
     private string last_name;
     private double salary;
     private double withheld;
-    private double salary_total;
+    private double total_salary;
 
-    public SalaryInfo(string last_name)
-    {
-        this.last_name = last_name;
-    }
+    public double Salary { get { return salary; } }
+    public double Withheld { get { return withheld; } }
+    public double Total_Salary { get { return total_salary; } }
 
     public SalaryInfo(string last_name, double salary, double withheld)
     {
@@ -45,9 +89,19 @@ public class SalaryInfo
         this.withheld = withheld;
     }
 
-    public double CountTotalSalary(int a)
+    public double TotalSalary()
     {
-        return salary - withheld;
+        this.total_salary = this.salary - this.withheld;
+        return this.total_salary;
     }
 
+    public void PrintInfo()
+    {
+        Console.WriteLine("{0}'s salary is {1}grn. Were withheld {2}grn. And the total salary is {3}grn", this.last_name, this.salary, this.withheld, this.total_salary);
+    }
+
+    public void PrintGeneralInfo()
+    {
+        Console.WriteLine("\n {0} --- {1} --- {2}grn --- {3}grn --- {4}grn", this.num, this.last_name, this.salary, this.withheld, this.total_salary);
+    }
 }
