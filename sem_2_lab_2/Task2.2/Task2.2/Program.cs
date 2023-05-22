c18d8170a921548501cf5a01f17bff160a465b84
IlliaBonkovskyi <bonkovskyiillia@gmail.com>
2023-05-22 23:17:44 +0300

    Task2.2 v1.0

---
+++ sem_2_lab_2/Task2.2/Task2.2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Vessel sailingVessel = new SailingVessel();
+        Vessel submarine = new Submarine();
+
+        Console.WriteLine("Sailing Vessel:");
+        sailingVessel.PrepareToMovement();
+        sailingVessel.Move();
+
+        Console.WriteLine("\nSubmarine:");
+        submarine.PrepareToMovement();
+        submarine.Move();
+    }
+}
+public abstract class Vessel
+{
+    public abstract void PrepareToMovement();
+    public abstract void Move();
+
+}
+
+public class SailingVessel : Vessel
+{
+    public override void PrepareToMovement()
+    {
+        Console.WriteLine("*The sailing vessel is preparing to move*");
+    }
+
+    public override void Move()
+    {
+        Console.WriteLine("*The sailing vessel is moving*");
+    }
+}
+
+public class Submarine : Vessel
+{
+    public override void PrepareToMovement()
+    {
+        Console.WriteLine("*The submarine is preparing to move*");
+    }
+
+    public override void Move()
+    {
+        Console.WriteLine("*The submarine is moving*");
+    }
+}
