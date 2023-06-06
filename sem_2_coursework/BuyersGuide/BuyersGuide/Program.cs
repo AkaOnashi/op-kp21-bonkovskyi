e182ea2d830a67bf5c3ad0d39e6598b14236b5ee
IlliaBonkovskyi <bonkovskyiillia@gmail.com>
2023-06-06 22:30:59 +0300

    Program v0.2

--- sem_2_coursework/BuyersGuide/BuyersGuide/Program.cs
+++ sem_2_coursework/BuyersGuide/BuyersGuide/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace BuyersGuide
 {
@@ -10,22 +11,165 @@ namespace BuyersGuide
     {
          static void Main(string[] args)
          {
+            //list with ALL shop
+            List<Shop> list_shop = new List<Shop>();
+
+            //encoding to Ukrainian letters
             Console.OutputEncoding = Encoding.Unicode;
-            ToyShop shop_1 = new ToyShop();
-            GroceryShop shop_2 = new GroceryShop();
+
+            bool flag = true;
+            int choice = 0;
+            Console.WriteLine(" Привіт!\nВітаю в програмі \"Довідник покупця\"");
+            while (flag)
+            {
+                Console.WriteLine("\nОсь чим наш довідник може допомогти: \n 1 - Додати магазин.\n 2 - Подивитися інформацію про всі магазини.\n 3 - Знайти магазин.\n 4 - Видалити магазин.\n 5 - Конвертувати інформацію у файл.\n 6 - Припинити програму.");
+                Console.Write("Обери номер опції: ");
+                choice = Convert.ToInt32(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("\nОбери спеціалізацію магазину, який хочеш додати:\n - Продукти;\n - Промислові товари;\n - Іграшки;\n - Біжутерія;\n - Книги;\n - Одяг;\n - Кафе;\n - Ресторан;\n - Універсам;\n - Супермаркет;\n - Торговий центр;");
+                        Console.Write("\nНапиши тип спеціалізації: ");
+                        string spec_choice = Console.ReadLine();
+                        Console.Write("\nВведіть назву магазину: ");
+                        string name_shop = Console.ReadLine();
+                        Console.Write("\nВведіть адресу магазину: ");
+                        string address_shop = Console.ReadLine();
+                        Console.Write("\nВведіть час роботи: ");
+                        string time_shop = Console.ReadLine();
+
+                        //Іфами визначаю, який вид магазину створити
+                        if (spec_choice.ToLower() == "продукти")
+                        {
+                            GroceryShop shop = new GroceryShop();
+                            shop.AddShop(name_shop, address_shop, time_shop);
+                            list_shop.Add(shop);
+                        }
+                        else if (spec_choice.ToLower() == "промислові товари")
+                        {
+                            IndusrialShop shop = new IndusrialShop();
+                            shop.AddShop(name_shop, address_shop, time_shop);
+                            list_shop.Add(shop);
+                        }
+                        else if (spec_choice.ToLower() == "іграшки")
+                        {
+                            ToyShop shop = new ToyShop();
+                            shop.AddShop(name_shop, address_shop, time_shop);
+                            list_shop.Add(shop);
+                        }
+                        else if (spec_choice.ToLower() == "біжутерія")
+                        {
+                            JewelleryShop shop = new JewelleryShop();
+                            shop.AddShop(name_shop, address_shop, time_shop);
+                            list_shop.Add(shop);
+                        }
+                        else if (spec_choice.ToLower() == "книги")
+                        {
+                            BookShop shop = new BookShop();
+                            shop.AddShop(name_shop, address_shop, time_shop);
+                            list_shop.Add(shop);
+                        }
+                        else if (spec_choice.ToLower() == "одяг")
+                        {
+                            ClothesShop shop = new ClothesShop();
+                            shop.AddShop(name_shop, address_shop, time_shop);
+                            list_shop.Add(shop);
+                        }
+                        else if (spec_choice.ToLower() == "кафе")
+                        {
+                            Cafe shop = new Cafe();
+                            shop.AddShop(name_shop, address_shop, time_shop);
+                            list_shop.Add(shop);
+                        }
+                        else if (spec_choice.ToLower() == "ресторан")
+                        {
+                            Restaurant shop = new Restaurant();
+                            shop.AddShop(name_shop, address_shop, time_shop);
+                            list_shop.Add(shop);
+                        }
+                        else if (spec_choice.ToLower() == "універсам")
+                        {
+                            Market shop = new Market();
+                            shop.AddShop(name_shop, address_shop, time_shop);
+                            list_shop.Add(shop);
+                        }
+                        else if (spec_choice.ToLower() == "супермаркет")
+                        {
+                            Supermarket shop = new Supermarket();
+                            shop.AddShop(name_shop, address_shop, time_shop);
+                            list_shop.Add(shop);
+                        }
+                        else if (spec_choice.ToLower() == "торговий центр")
+                        {
+                            ShoppingCenter shop = new ShoppingCenter();
+                            shop.AddShop(name_shop, address_shop, time_shop);
+                            list_shop.Add(shop);
+                        }
+                        else
+                        {
+
+                        }
+                        Console.WriteLine("\nВаш магазин додано!");
+                        break;
+                    case 2:
+                        Console.WriteLine("\nОсь інформація про магазини:");
+                        foreach (Shop el in list_shop)
+                        {
+                            el.PrintInfo();
+                        }
+                        break;
+                    case 3:
+                        Console.Write("\nПошук магазину.\nВведіть назву магазину: ");
+                        string find_name = Console.ReadLine();
+                        Console.WriteLine("\nШуканий магазин: ");
+                        foreach(Shop el in list_shop)
+                        {
+                            if(find_name == el.Name)
+                                el.PrintInfo();
+                        }
+                        break;
+                    case 4:
+                        Console.Write("\nДавайте видалимо магазин.\nВведіть назву магазину, який хочете видалити: ");
+                        string del_name = Console.ReadLine();
+                        for (int i = list_shop.Count - 1; i >= 0; i--)
+                        {
+                            if (del_name == list_shop[i].Name)
+                                list_shop.RemoveAt(i);
+                        }
+                        break;
+                    case 5:
+                        string filePath = "D:\\gitreps\\op\\op-kp21-bonkovskyi\\sem_2_coursework\\Buyers_Guide.csv";
+                        using (StreamWriter sr = new StreamWriter(filePath, append: true, encoding: Encoding.UTF8))
+                        {
+                            foreach(Shop el in list_shop)
+                            {
+                                string text = $"{el.Name};{el.Address};{el.ukrTypeShop};{el.WorkingHours}";
+                                sr.WriteLine(text);
+                            }
+                        }
+                        Console.WriteLine("\nІнформація про магазини конвертована в файл!");
+                        break;
+                    case 6:
+                        Console.WriteLine("\nРобота програми призупинена.");
+                        flag = false;
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+            
 
             //StreamWriter sw = new StreamWriter("D:\\gitreps\\op\\op-kp21-bonkovskyi\\sem_2_coursework\\Buyers_Guide.csv");
-            //sw.WriteLine();
-            //sw.WriteLineAsync("Faid; Close; Or; Open;");
-            //sw.WriteLine("Kali; Daorl; Pear;");
             //sw.Close();
-            
+
             //shop_1.Name = "Будинок ірашок";
             //Console.WriteLine(shop_1.Name);
 
-            shop_1.AddShop("Будинок іграшок", "вул. Хрещатик 1", Ownership.Corporation, "5:00 - 21:00");
-            shop_1.PrintInfo();
-         }
+            //shop_1.AddShop("Будинок іграшок", "вул. Хрещатик 1", Ownership.Corporation, "5:00 - 21:00");
+            //shop_1.PrintInfo();
+        }
         
     }
 }
--- sem_2_coursework/BuyersGuide/BuyersGuide/Shop.cs
+++ sem_2_coursework/BuyersGuide/BuyersGuide/Shop.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace BuyersGuide
 {
-    public enum Ownership
+    public enum Ownership 
     {
         Solo,
         Partenrship,
@@ -29,12 +29,14 @@ namespace BuyersGuide
     }
     public abstract class Shop
     {
+
         private string name;
         private string address;
         public TypeShop typeShop;
+        public string ukrTypeShop;
         private string workingHours;
         private Ownership ownership;
-
+ 
         public string Name { get { return name; } set { name = value; } }
         public string Address { get { return address; } set { address = value; } }
         public string WorkingHours { get { return workingHours; } set { workingHours = value; } }
@@ -45,9 +47,16 @@ namespace BuyersGuide
         {
             this.typeShop = typeShop;
         }
-        
+        public void AddShop(string name, string address, string workingHours)
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+            this.name = name;
+            this.address = address;
+            this.workingHours = workingHours;
+        }
         public void AddShop(string name, string address, Ownership ownership, string workingHours)
         {
+            Console.OutputEncoding = Encoding.Unicode;
             this.name = name;
             this.address = address;
             this.ownership = ownership;
@@ -56,7 +65,44 @@ namespace BuyersGuide
 
         public void PrintInfo()
         {
-            Console.WriteLine(this.Name + this.Address + this.Ownership + this.typeShop + this.WorkingHours);
+            Console.OutputEncoding = Encoding.Unicode;
+            switch (typeShop)
+            {
+                case TypeShop.Grocery:
+                    ukrTypeShop = "Продуктовий магазин";
+                    break;
+                case TypeShop.Industrial:
+                    ukrTypeShop = "Промисловий магазин";
+                    break;
+                case TypeShop.Pet:
+                    ukrTypeShop = "Зоо-магазин";
+                    break;
+                case TypeShop.Book:
+                    ukrTypeShop = "Книгарня";
+                    break;
+                case TypeShop.Jewellery:
+                    ukrTypeShop = "Біжутерія";
+                    break;
+                case TypeShop.Clothes:
+                    ukrTypeShop = "Магазин одягу";
+                    break;
+                case TypeShop.Toy:
+                    ukrTypeShop = "Іграшковий магазин";
+                    break;
+                case TypeShop.Cafe:
+                    ukrTypeShop = "Кафе";
+                    break;
+                case TypeShop.Market:
+                    ukrTypeShop = "Універсам";
+                    break;
+                case TypeShop.ShoppingCenter:
+                    ukrTypeShop = "Торговий центр";
+                    break;
+            }
+          
+            
+            Console.WriteLine("\"{0}\" -- {1} -- {2} -- {3}", this.Name, this.Address, this.ukrTypeShop, this.WorkingHours);
+            //this.Ownership + 
         }
     }
 }
